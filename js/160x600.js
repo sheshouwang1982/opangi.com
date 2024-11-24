3a0ace4bee795514a1e320e23c42315dc77c4bb0
sheshouwang1982 <sheshouwang@hotmail.com>
2024-11-24 14:50:17 +0800

    Add files via upload

---
+++ js/160x600.js
@@ -0,0 +1,11 @@
+document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- 160*600 -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;width:160px;height:600px\'");
+document.writeln("     data-ad-client=\'ca-pub-1233302309770573\'");
+document.writeln("     data-ad-slot=\'2618502081\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+
+
