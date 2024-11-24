509d34993fbf3e48e8b71ba304889605d06201c2
sheshouwang1982 <sheshouwang@hotmail.com>
2024-11-24 14:49:22 +0800

    Add files via upload

---
+++ php/jz.js
@@ -0,0 +1,6 @@
+document.getElementById("userad1").innerHTML = document.getElementById("myads2").innerHTML; 
+document.getElementById("myads2").innerHTML = ""; 
+document.getElementById("userad2").innerHTML = document.getElementById("s-gg1").innerHTML; 
+document.getElementById("s-gg1").innerHTML = ""; 
+document.getElementById("userad3").innerHTML = document.getElementById("gg-header").innerHTML; 
+document.getElementById("gg-header").innerHTML = ""; 
