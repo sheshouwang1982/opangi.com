9a95244ea9e9a02b53f1ff96638fa886edad17f3
sheshouwang1982 <sheshouwang@hotmail.com>
2025-07-04 15:57:32 +0800

    Delete js/250x250.js

--- js/250x250.js
+++
@@ -1,9 +0,0 @@
-document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
-document.writeln("<!-- 250*250 -->");
-document.writeln("<ins class=\'adsbygoogle\'");
-document.writeln("     style=\'display:inline-block;width:250px;height:250px\'");
-document.writeln("     data-ad-client=\'ca-pub-1233302309770573\'");
-document.writeln("     data-ad-slot=\'2207609981\'></ins>");
-document.writeln("<script>");
-document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
-document.writeln("</script>");
